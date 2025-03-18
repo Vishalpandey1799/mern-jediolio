8762af7ecdce657f33611f04abcbe11c80548f70
vishal kumar <130959006+Vishalpandey1799@users.noreply.github.com>
2025-03-18 09:56:55 +0530

    project completed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json())
  
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 app.use(express.json())
 app.use(cors({
     origin: "http://localhost:5173",
