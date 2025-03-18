b7129d2bfdea87bdc0214b8a782fa398b6e6e4d7
vishal kumar <130959006+Vishalpandey1799@users.noreply.github.com>
2025-03-18 21:24:51 +0530

    y

--- Frontend/src/Component/Footer/Footer.jsx
+++ Frontend/src/Component/Footer/Footer.jsx
@@ -2,11 +2,8 @@ const Footer = () => {
     return (
         <footer className="bg-gradient-to-r from-gray-900 to-fuchsia-500 text-white py-6 text-center w-full mt-auto">
             <p className="text-lg font-semibold">
-                Made with <span className="text-red-500 animate-pulse">❤</span> by
+                Made with <span >Aura++</span> by
                 <span className="font-bold "> Vishal </span>
-                <span className="text-gray-900">&lt;&gt;</span>
-                <span className="font-bold"> Code Jedi </span>
-                <span className="text-gray-900">&lt;/&gt;</span>
             </p>
         </footer>
     );
