b8b48e4a5a66bb393e6bae17aa2351b75cc7ab76
vishal kumar <130959006+Vishalpandey1799@users.noreply.github.com>
2025-07-20 21:32:45 +0530

    Update Footer.jsx

--- Frontend/src/Component/Footer/Footer.jsx
+++ Frontend/src/Component/Footer/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="bg-gradient-to-r from-gray-900 to-fuchsia-500 text-white py-6 text-center w-full mt-auto">
             <p className="text-lg font-semibold">
                 Made with <span >Aura++</span> by
-                <span className="font-bold "> Vishal </span>
+ 
             </p>
         </footer>
     );
